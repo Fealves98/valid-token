1015528928dfce44b73cabd43fe27fc042399b69
Fealves98 <fealves.silva98@gmail.com>
2024-03-21 00:07:31 -0300

    Inserção do @valid na controller

--- src/main/java/br/com/itau/authentication/application/in/controller/TokenController.java
+++ src/main/java/br/com/itau/authentication/application/in/controller/TokenController.java
@@ -3,6 +3,7 @@ package br.com.itau.authentication.application.in.controller;
 import br.com.itau.authentication.application.facade.TokenFacade;
 import br.com.itau.authentication.domain.models.TokenRequest;
 import br.com.itau.authentication.domain.models.TokenResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class TokenController {
 
     @ResponseStatus(OK)
     @PostMapping("/token")
-    public TokenResponse validToken(@RequestBody TokenRequest request) {
+    public TokenResponse validToken(@RequestBody @Valid TokenRequest request) {
         log.info("------------------------");
         return facade.validToken(request);
     }
